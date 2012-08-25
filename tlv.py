5c08d991ce33fc3fa8a800c94802c813c79c2aab
apuigsech <albert@puigsech.com>
2012-08-25 17:03:31 +0200

    redesign of tlv - now with full tlv support

--- tlv.py
+++ tlv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-#    Python TLV Parser (as part of EMV Framework)
+#    Python TLV (as part of EMV Framework)
 #    Copyrigh 2012 Albert Puigsech Galicia <albert@puigsech.com>
 #
 #    This code is free software; you can redistribute it and/or
@@ -10,124 +10,106 @@
 #    of the License, or (at your option) any later version.
 #
 
-def tag_tags(data, size, tags_db):
-	i = 0
-	items = []
+TAG_CLASS_UNIVERSAL = 0x0
+TAG_CLASS_APLICATION = 0x1
+TAG_CLASS_CONTEXT_SPECIFIC= 0x2
+TAG_CLASS_PRIVATE = 0x3
 
-	while (i < size):
-		tag_1 = data[i]
-		tag_2 = data[i+1] + 256 * data[i]
+TAG_TYPE_PRIMITIVE = 0x0
+TAG_TYPE_CONSTRUCTED = 0x1
 
-		if tags_db.has_key(tag_2):
-			i += 2
-			tag = tag_2
-		elif tags_db.has_key(tag_1):
-			i += 1
-			tag = tag_1
-		else:
-			i += 1
-			tag = tag_1
-	
-		tag_size = data[i]
-		if (tag_size == 0x81):
-			i += 1
-			tag_size = data[i]
-		i += 1
-		tag_data = data[i:i+tag_size]
+TAG_SIZE_BIG_1 = 0x81
+TAG_SIZE_BIG_2 = 0x82
 
-		if (tags_db.has_key(tag)):	   
-			tag_name = tags_db[tag][0]	
-			tag_func = tags_db[tag][1]
-		else:
-			tag_name = 'Unknown tag'
-			tag_func = tag_binary
+# TODO: Implements class specifcations.
 
-
-		print '0x%x [0x%.2x] %s' % (tag, tag_size, tag_name)
-		items.append({
-			'name':tag_name,
-			'size':tag_size,
-			'data':tag_data,
-			'content':tag_func(tag_data, tag_size, tags_db)
-		})
-
-		i += tag_size
-
-	return items
-	
-	
-def tag_binary(data, size, tags_db):
-	i = 0
-
-	if (len(data) < size):
-		print "PARSE ERROR"
-		size = len(data)
-
-	while (i < size):
-		print '%.2x' % (data[i]),
-		i += 1
-	print
-
-
-# TODO
-def tag_numeric(data, size, tags_db):
-	i = 0
-
-
-def tag_string(data, size, tags_db):
-	i = 0
-	while (i < size):
-		print '%c' % (data[i]),
-		i += 1
-	print
-
-def tag_object(data, size, tags_db):
-	i = 0
-	while(i < size):
-		tag_1 = data[i]
-		tag_2 = data[i+1] + 256 * data[i]
-
-		if tags_db.has_key(tag_1):
-			i += 1
-			tag = tag_1
-		elif tags_db.has_key(tag_2):
-			i += 2
-			tag = tag_2
-		else:
-			i += 1
-			tag = tag_1
-
-		tag_size = data[i]
-		i += 1
-
-                if (tags_db.has_key(tag)):
-                        tag_name = tags_db[tag][0]
-                        tag_func = tags_db[tag][1]
-                else:
-                        tag_name = 'Unknown tag'
-                        tag_func = tag_binary
-
-                print ' * 0x%x [0x%.2x] %s' % (tag, tag_size, tag_name)
-
-TAG_TAGS = tag_tags
-TAG_BINARY = tag_binary
-TAG_NUMERIC = tag_numeric
-TAG_STRING = tag_string
-TAG_OBJECT = tag_object
-
-# TODO: Create a tree with the information but not print it.
-
-class TLV:
-	def __init__(self, tags_db={}):
-		self.tags_db = tags_db
-		self.data = []
+class TAG:
+	def __init__(self, data=None):
+		self.childs = [] 
+		self.root = False
+		self.code = None
+		self.type = None
+#		self.class = None
+		self.extended = None
+		self.size = None
+		self.total_size = None
+		self.parse(data)
 
 	def parse(self, data):
-		self.data = tag_tags(data, len(data), self.tags_db)
-		return self.data
-
-	def show(self, data=None, deep=0):
 		if data == None:
-			data = self.data
-		for i in data:
-			print i
+			return
+		
+		size = len(data)
+		
+		i = 0
+		if data[i]&0b00011111 == 0b00011111:
+			self.extended = True
+		else:
+			self.extended = False
+#		self.class = (data[i]&0b11000000)>>6
+		self.type = (data[i]&0b00100000)>>5		
+
+		if self.extended:
+			self.code = 256 * data[i] + data[i+1]
+			i += 2
+		else:
+			self.code = data[i]	
+			i += 1
+
+		if data[i] == TAG_SIZE_BIG_1:
+			self.size = data[i+1]
+			i += 2
+		elif data[i] == TAG_SIZE_BIG_2:
+			self.size = 256 * data[i+1] + data[i+2]
+			i +=3
+		else:
+			self.size = data[i]
+			i += 1
+
+		self.data = data[i:i+self.size]
+		i += self.size
+
+		if self.type == TAG_TYPE_CONSTRUCTED:
+			j = 0
+			while j < self.size:
+				tag = TAG(self.data[j:])
+				self.childs.append(tag)
+				j += tag.total_size
+
+		self.total_size = i
+
+
+	def list_childs(self, code=None):
+		if code == None:
+			return self.childs
+		ret = []
+		for c in self.childs:
+			if c.code == code:
+				ret.append(c)
+		return ret
+
+	def show(self, deep=0):
+		if self.root:
+			self.childs[0].show(deep)
+		else:
+			deep_str = deep*'   '
+			print '%s%.2x [%.2x]' % (deep_str, self.code, self.size)
+			if self.type == TAG_TYPE_PRIMITIVE:
+				print '%s  ' % (deep_str), 
+				for i in self.data:
+					print '%.2x' % (i),
+				print
+			deep += 1
+			for tag in self.childs:
+				tag.show(deep)
+
+class TLV(TAG):
+	def parse(self, data):
+		size = len(data)
+		self.root = True
+                self.type = TAG_TYPE_CONSTRUCTED
+		i = 0
+		while i < size:
+			tag = TAG(data[i:])
+			self.childs.append(tag)
+			i += tag.total_size
