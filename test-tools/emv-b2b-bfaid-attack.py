d491bc5fb36be770ed62b29e4c9d48f5f159d1bf
apuigsech <albert@puigsech.com>
2012-12-06 06:17:55 +0100

    add experimental EMV byte to byte bruteforce AID attack tool

---
+++ test-tools/emv-b2b-bfaid-attack.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from emv import *
+import sys
+
+
+def check_aid(aid):
+	for plus in range(0x00,0xff+1):
+		current_aid = aid + [plus]
+
+		apdu_res = emv.SELECT_AID(current_aid)
+
+		sys.stdout.write("\r\033[KCHECK      : {0:s}".format(map(hex,current_aid)))
+		sys.stdout.flush()
+
+		if (apdu_res.sw1 == 0x90):
+			tlv=TLV(apdu_res.data)
+			final_aid = tlv.list_childs(0x6f)[0].list_childs(0x84)[0].data
+			if (final_aid == current_aid):
+				sys.stdout.write(" (FOUND!)\n".format(map(hex,current_aid)))
+				tlv.show()
+				check_aid(current_aid)
+			else:
+				check_aid(current_aid)
+
+
+emv = EMV()
+
+check_aid([])
